2e638b5be4ae756744340dc749c6a50a5af24342
Bryan CADET <bryan.cadet@protonmail.com>
2023-03-08 11:03:15 +0000

    remove ram func test

--- project/src/main.c
+++ project/src/main.c
@@ -6,11 +6,6 @@
 #include "hardware/clocks.h"
 #include <stdio.h>
 
-void __not_in_flash_func(some_function_name)(void) {
-    sleep_ms(1000);
-}
-
-
 int main()
 {
     stdio_usb_init();
@@ -23,26 +18,16 @@ int main()
     pwm_config config = pwm_get_default_config();
     printf("\r\nclock_get_hz(clk_sys)=%d", clock_get_hz(clk_sys));
     pwm_config_set_clkdiv(&config, 38);
-    // pwm_config_set_wrap(&config, 9842);
     pwm_init(slice_num, &config, true);
 
     modbus_init();
     rtc_init();
-    printf("\r\nsome_function_name=0x%X", some_function_name);
-    printf("\r\nmodbus_init=0x%X", modbus_init);
-
 
     while(true)
     {
         platform_modbus_usb_cdc_xfer();
         rtc_update();
         pwm_set_chan_level(slice_num, 0, 3276 );
-        some_function_name();
-        // sleep_ms(500);
-        // pwm_set_chan_level(slice_num, 0, 6553 );
-        // sleep_ms(500);
-
-
     }
     return 0;
 }
