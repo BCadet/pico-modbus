2d160de1a1b18312cad6306825bfb393d18ff4b6
Bryan Cadet <bryan.cadet@advans-group.com>
2023-03-22 14:30:41 +0000

    update only gpio inputs

--- project/src/gpios.c
+++ project/src/gpios.c
@@ -10,7 +10,7 @@ void gpios_update(uint32_t directions, uint32_t pulls, uint32_t *values)
         gpio_set_pulls(i, (pulls & (1<<i)), !(pulls & (1<<i)));
     }
     gpio_put_masked(directions, *values);
-    *values = gpio_get_all();
+    *values = ~directions & gpio_get_all();
 }
 
 void gpios_init(uint32_t mask)
