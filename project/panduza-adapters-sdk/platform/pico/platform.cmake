4de0ce47fd59283fd8d8791c15f2533b7d5ba09a
Bryan CADET <bryan.cadet@protonmail.com>
2023-03-24 11:26:03 +0000

    fix cmake dependencies

--- project/panduza-adapters-sdk/platform/pico/platform.cmake
+++ project/panduza-adapters-sdk/platform/pico/platform.cmake
@@ -29,7 +29,14 @@ macro(panduza_platfrom_init TARGET)
     pico_find_compiler(PICO_COMPILER_GDB arm-none-eabi-gdb)
     set(CMAKE_DEBUGGER ${PICO_COMPILER_GDB} CACHE FILEPATH "path to gdb")
 
-    target_compile_definitions(panduza_platform_pico PRIVATE
+    target_compile_definitions(${TARGET} PUBLIC
+        -DCFG_TUSB_CONFIG_FILE="pico_tusb_config.h"
+        -DPICO_STDIO_USB_CONNECT_WAIT_TIMEOUT_MS=-1 # wait in stdio_usb_init until usb is connected
+        # -DPICO_STDIO_USB_POST_CONNECT_WAIT_DELAY_MS=1000 # wait 1s AFTER the usb is conected
+        -DPICO_STDIO_USB_LOW_PRIORITY_IRQ=0x1f
+    )
+
+    target_compile_definitions(panduza_platform_pico PUBLIC
         -DCFG_TUSB_CONFIG_FILE="pico_tusb_config.h"
         -DPICO_STDIO_USB_CONNECT_WAIT_TIMEOUT_MS=-1 # wait in stdio_usb_init until usb is connected
         # -DPICO_STDIO_USB_POST_CONNECT_WAIT_DELAY_MS=1000 # wait 1s AFTER the usb is conected
