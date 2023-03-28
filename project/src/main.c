19db5a2d7c204afaa47f87671d64e11645087520
Bryan Cadet <bryan.cadet@advans-group.com>
2023-03-28 13:28:15 +0000

    add debug flag to task

--- project/src/main.c
+++ project/src/main.c
@@ -11,7 +11,16 @@ int main()
 
     // init the panduza dio modbus device and regs + bind
     pza_dio_t dio = {0};
-    const uint32_t gpioMask = ~((1 << 31) | (1 << 30) | (1 << 29) | (1 << 24) | (1 << 23) | (1 << 1) | (1 << 0)); // disable io 31/30/29/24/23/1/0
+    const uint32_t gpioMask = 
+        ~((1 << 31) 
+        | (1 << 30) 
+        | (1 << 29) 
+        | (1 << 24) 
+        | (1 << 23) 
+        | (1 << 15) 
+        | (1 << 14) 
+        | (1 << 01) 
+        | (1 << 00)); // disable io 31/30/29/24/23/1/0
     pza_dio_init(&dio, gpioMask);
 
     // init the panduza uart modbus device and regs + bind
