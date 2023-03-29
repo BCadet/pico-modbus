f20a92c7bb73505309f1a64115244984851a92d8
Bryan Cadet <bryan.cadet@advans-group.com>
2023-03-29 09:43:05 +0000

    test i2c

--- project/src/main.c
+++ project/src/main.c
@@ -1,7 +1,6 @@
-#include "panduza/dio.h"
+#include "panduza/i2c.h"
 #include "panduza/interface.h"
 #include "panduza/platform.h"
-#include "panduza/uart.h"
 #include <stdio.h>
 
 int main()
@@ -9,29 +8,13 @@ int main()
     pza_platform_init();
     pza_interface_init();
 
-    // init the panduza dio modbus device and regs + bind
-    pza_dio_t dio = {0};
-    const uint32_t gpioMask = 
-        ~((1 << 31) 
-        | (1 << 30) 
-        | (1 << 29) 
-        | (1 << 24) 
-        | (1 << 23) 
-        | (1 << 15) 
-        | (1 << 14) 
-        | (1 << 01) 
-        | (1 << 00)); // disable io 31/30/29/24/23/1/0
-    pza_dio_init(&dio, gpioMask);
-
-    // init the panduza uart modbus device and regs + bind
-    pza_uart_t uart = {.control.content.baudrate = 115200};
-    pza_uart_init(&uart);
+    pza_i2c_t i2c;
+    pza_i2c_init(&i2c);
 
     while (1)
     {
         pza_interface_run();
-        pza_dio_run(&dio);
-        pza_uart_run(&uart);
+        pza_i2c_run(&i2c);
     }
     return 0;
 }
